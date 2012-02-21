9556fff3106fa4943c31979c18c01e68f74a7874
Antoine Laroche <antoine_laroche7@hotmail.fr>
2012-02-21 16:45:06 -0500

    Ajout des plugins jquery pour menu et slider. Ajout squelette.php mais juste pour le menu

---
+++ squelette.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+                   
+<html>
+	<head>
+	<!-- informations sur le document -->
+		 <meta name="language" content="fr">
+		 <meta name="author" content="Laroche Antoine & Maurin Anthony">
+		 <meta name="subject" content="Ressources naissance">
+		 <meta name="description" content="Un éventail de services et d’activités pour les futurs parents afin de répondre aux nombreux besoins durant la période prénatale et pour les familles durant la première année du nouveau-né.">
+		 <meta http-equiv="content-type" content="text/html; charset=utf-8">
+		 <title>Ressources-naissances</title>
+		 <link rel="stylesheet" type="text/css" href="style.css">
+		 <link rel="stylesheet" type="text/css" media="screen" href="jquery.ui.potato.menu.css" />
+		 <script type="text/javascript" src="jquery.nivo.slider.js"></script>
+		 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.ui.potato.menu.js"></script>
+		<script type="text/javascript">
+			(function($) 
+			{
+				$(document).ready(function()
+				{
+					$('#menu1').ptMenu();
+				});
+			})(jQuery);
+		</script>
+	</head>
+	<body>
+	<!-- corps du document -->
+		<div id="banniere">
+		<h1> Ressources-naissances banniere</h1>
+		</div>
+		<div id="contenu">
+			<div id="titre">
+			<h2>Accueil ressources-naissances</h2>
+			</div>
+			<div id="menu-bas">
+
+				<ul id="menu1">
+					<li><a href="#">ACCUEIL</a></li>
+					<li>
+						<a href="#">À PROPOS DE NOUS</a>
+						<ul>
+							<li><a href="#">Mission</a></li>
+							<li><a href="#">Historique</a></li>
+							<li><a href="#">Conseil d'administration</a></li>
+							<li><a href="#">Notre équipe</a></li>
+							<li><a href="#">Membres</a></li>
+							<li><a href="#">Bénévoles</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">ATELIERS/ACTIVITÉS</a>
+						<ul>
+							<li><a href="#">BÉBÉ S'EN VIENT</a></li>
+							<li><a href="#">BÉBÉ EST ARRIVÉ</a></li>
+							<li><a href="#">Cafés-rencontres</a></li>
+							<li><a href="#">Espace allaitement</a></li>
+							<li><a href="#">Balado poussette</a></li>
+							<li><a href="#">Conférences</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="#">SERVICES</a>
+						<ul>
+							<li><a href="#">Consultation individuelle en allaitement</a></li>
+							<li><a href="#">Consultation téléphone</a></li>
+							<li><a href="#">Marrainage en allaitement</a></li>
+						</ul>
+					</li>
+					<li><a href="#">ASSISTANCE PÉRINATALE À DOMICILE</a></li>
+					<li>
+						<a href="#">PARTENARIAT</a>
+						<ul>
+							<li><a href="#">Partenaires financiers</a></li>
+							<li><a href="#">Organismes partenaires</a></li>
+							<li><a href="#">Participation à des regroupements</a></li>
+						</ul>
+					</li>
+					<li><a href="#">NOUS SOUTENIR</a></li>
+					<li><a href="#">BÉNÉVOLAT</a></li>
+					<li>
+						<a href="#">DOCUMENTATION</a>
+						<ul>
+							<li><a href="#">Articles</a></li>
+							<li><a href="#">Publications</a></li>
+						</ul>
+					</li>
+					<li><a href="#">NOUS JOINDRE</a></li>
+					<li><a href="#">LIENS UTILES</a></li>
+
+				</ul>
+			</div>
+			<div id="menu-droite">
+		
+			</div>
+		</div>
+	</body>
+</html>
