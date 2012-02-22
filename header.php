a61a1add01d6c3ff521d868f6513c61af33957b2
Jonathan Pouliot <pouliot.jonathan@gmail.com>
2012-02-22 14:19:27 -0500

    $title = $titre

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>
 			<?php
-				echo isset($title) ? $title : 'Ressources-Naissances';
+				echo isset($titre) ? $titre : 'Ressources-Naissances';
 			?>
 		</title>
 		<link media="screen" REL="stylesheet" TYPE="text/css" href="css/styles.css">
