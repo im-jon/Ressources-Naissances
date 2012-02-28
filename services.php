bc28312a42cfdb6b4a56870d8202a4e9d7dce12b
Corentin Beauvillain <drazil@hotmail.fr>
2012-02-28 12:59:23 -0500

    derniere maj Corentin

---
+++ services.php
@@ -0,0 +1,92 @@
+<?php 
+$titre = "Bénévolat";
+include("header.php"); ?>
+		 
+		 <link rel="stylesheet" type="text/css" href="css/styles.css">
+		 <link rel="stylesheet" type="text/css" media="screen" href="jquery.ui.potato.menu.css" />
+
+			
+					 <script type="text/javascript" src="jquery.nivo.slider.js"></script>
+		 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.ui.potato.menu.js"></script>
+		<script type="text/javascript" src="js/afficherCacher.js"></script>
+
+
+			<div id="texte">
+			<p class="titreTexte">
+			<STRONG>Assistance à domicile</STRONG>&emsp;&emsp;&emsp;(clic ici pour en apprendre plus)
+			</p>
+			<div class="contenuTexte">
+			<p id=préambule>
+			Vous avez besoin d'un peu de répit, d'un peu d'aide à la maison ? Vous manquez de temps et/ou d'énergie ? Une assistante périnatale ("une grand-maman sur mesure") peut vous offrir du soutien à domicile, prendre soin du nouveau-né ou des autres enfants, vous apporter une aide pour l’allaitement, les soins au bébé, effectuer de légères tâches ménagères et préparer de bons petits plats. Un ou deux services par semaine peuvent faire toute la différen
+			</p>
+			<p id=préambule>
+				<fieldset><P><STRONG>Une assistante, c'est une femme qui :</STRONG></P>
+				<UL>
+				<LI>a été sélectionnée selon les exigences de Ressources-Naissances; 
+				<LI>a des aptitudes en relation d'aide et une formation continue en ce qui a trait à la périnatalité; 
+				<LI>adore les enfants et connaît bien les étapes de leur développement; 
+				<LI>est sensibilisée à la réalité des nouvelles familles.</LI></UL>
+				<P>Elle vous informe sur les ressources disponibles&nbsp;selon vos besoins.&nbsp; Elle vous écoute, vous supporte tout en vous permettant de prendre le répit dont vous avez besoin. </P>
+				<P><STRONG>À qui s'adresse ce service ?</STRONG></P>
+				<UL>
+				<LI>aux femmes enceintes ayant besoin de repos en période prénatale; 
+				<LI>aux familles, aux pères, aux mères durant la première année de vie de leur enfant.</LI></UL>
+			</p>
+			<p id=témoignage>
+			<STRONG>Témoignages :</STRONG>
+			<P>"L'énergie et la bonne humeur de l'assistante périnatale me donnent de l'énergie pour le reste de la semaine." </P>
+<P>"... Cette aide ne m'a pas seulement aidée dans mon rôle de mère, elle m'a aussi aidée à maintenir une bonne santé physique et mentale."</P>
+			</p>
+			<fieldset>
+			</div>
+			</div>
+			</br>
+
+			<div id="Possib-benev">
+				<p class="titreTexte">
+				<STRONG>Consultation individuelle en allaitement</STRONG>&emsp;&emsp;&emsp;(clic ici pour en apprendre plus)
+				</p>
+				<div class="contenuTexte">
+				<fieldset><P><U>Sur rendez-vous seulement</U>,&nbsp;possibilité d'une rencontre individuelle pour vous soutenir dans votre allaitement.</P>
+<P>Tarif : 20$ pour une heure de consultation.&nbsp; </P>
+
+<P>Du lundi au vendredi, aux heures de bureau.&nbsp; Voir la section 'Nous joindre'.</P><fieldset>
+				</div>
+			</div>
+			</br>
+
+			<div id="competences">
+				<p class="titreTexte">
+				Competences -> Ce que nous recherchons&emsp;&emsp;&emsp;(clic ici pour en apprendre plus)
+				</p>
+				<p class="contenuTexte">
+				Réanimation, masseur
+				</p>
+			</div>
+			</br>
+
+			<div id="formation">
+				<p class="titreTexte">
+				Formation -> Ce que nous pouvons vous apporter pour mieux aider&emsp;&emsp;&emsp;(clic ici pour en apprendre plus)
+				</p>
+				<p class="contenuTexte">
+				Soin aux nourrissons, aide à l'allaitement, ça s'apprend !
+				</p>
+			</div>
+			</br>
+
+			<div id="avantages">
+				<p class="titreTexte">
+				Le benevolat -> ca m'apporte quoi ?&emsp;&emsp;&emsp;(clic ici pour en apprendre plus)
+				</p>
+				<p class="contenuTexte">
+				Une expérience unique dans le milieu du nourisson
+				</p>
+			</div>
+			<div id="photos">
+			<img src="">
+			</div>
+		</div>
+	</body>
+</html>
