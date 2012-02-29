d12538c23636819bb2b203d7b93453f2e6bf39f2
Antoine Laroche <antoine_laroche7@hotmail.fr>
2012-02-29 17:48:48 -0500

    Page publication fini

--- Publications.php
+++ Publications.php
@@ -1,6 +1,30 @@
 <?php 
-$titre = "Publications";
-include("header.php"); ?>
+	$titre = "Publications";
+	include("header.php"); 
+	include("Actions//mysql.php");
 
+	 echo "<h1>Publications</h1>";
+	 //On va chercher dans les publications qui sont des gazette des pousettes
+	 $gazette = "SELECT nom,chemin FROM publications WHERE gazette = 1 ORDER BY id DESC;";
+	 $resultat = mysql_query($gazette);
+	 $i = 1;
+	 while($publications = mysql_fetch_array($resultat))
+	   {
+		if($i == 1)
+			echo '<h2>Dernière gazette des pousettes</h2></br><div class="titre"><a href="'. $publications["chemin"]. '">' . $publications["nom"] . "</a></br>";
+		else if($i == 2)
+			echo '<h2>Archive</h2><div class="titre"><a href="'. $publications["chemin"]. '">' . $publications["nom"] . "</a></br>";
+		else
+			echo '<div class="titre"><a href="'. $publications["chemin"]. '">' . $publications["nom"] . "</a></br>";
+		$i = $i + 1;
+	   }
+	   $i = 1;
+	   echo "<h2> Autre publications</h2></br>";
+	 $autrepub = "SELECT nom,chemin FROM publications WHERE gazette = 0 ORDER BY id DESC;";
+	 $resultat2 = mysql_query($autrepub);
+	 
+	 while($pub2 = mysql_fetch_array($resultat2))
+			echo '<div class="titre"><a href="'. $pub2["chemin"]. '">' . $pub2["nom"] . "</a></br>";
 	
-<?php include("footer.php"); ?>
+	mysql_close();
+	include("footer.php"); ?>
