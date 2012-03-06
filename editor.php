ddcee794e2153c578e9f8a4d23371bdc91dc65e0
Jonathan Pouliot <pouliot.jonathan@gmail.com>
2012-03-06 17:23:47 -0500

    Ajout de CKEditor

--- editor.php
+++ editor.php
@@ -1,28 +1,29 @@
+<?php
+include("ckeditor/ckeditor.php");
+$nom = $_REQUEST['nomfichierouvrir'];
+$lefichier = '';
+
+$fp = fopen($nom,"r");
+
+while(!feof($fp)) {
+	$lefichier .= fgets($fp, 4096);
+}
+
+fclose($fp);
+
+$CKEditor = new CKEditor();
+$CKEditor->basePath = 'ckeditor/';
+$CKEditor->config['height'] = '100%';
+
+?>
+
 <?php include("header.php"); ?>
 
 <div id="titre"><h2>Editez vos pages</h2></div>
 <center>
-	<?php include("ckeditor/ckeditor.php"); ?>
 	<form action="editor2.php" method="POST">
-		<?php
-			$nom=$_REQUEST['nomfichierouvrir'];
-			$lefichier = '';
-			$fp=fopen($nom,"r");//ouverture du fichier
-			while(!feof($fp))//on parcoure toute les lignes
-			{
-				$lefichier .= fgets($fp,4096);//lecture du contenu de la ligne
-			}
-			fclose($fp);
-
-			$oFCKeditor = new CKEditor();//nouvelle isntance de FCKeditor
-			$oFCKeditor->Width = '100%';
-			echo "<table border=1 width='85%' height='400'><tr><td>";
-			$oFCKeditor->editor("FCKeditor1", $lefichier);
-			echo "</td></tr></table>";
-			echo "Enregistrer le fichier sous:<input type='text' name='toto' size='20' value='".$nom."' disabled='disabled'><br/><br/>";
-			echo "<input type='hidden' name='nomfichier' value='".$nom."'>";
-		?>
-		
+		<?php $CKEditor->editor("CKEditor", $lefichier); ?>
+		<input type="hidden" name="nomfichier" value="<?= $nom ?>">		
 		<input type="submit" value="Enregistrer">
 	</form>
 	<br/><br/>
--- editor2.php
+++ editor2.php
@@ -1,17 +1,26 @@
-<?php include("header.php"); ?>
-			<div id="titre">
-			<h2>Editez vos pages</h2>
-			</div>
-			<center>
-				<?php
-					$nomfichier = $_REQUEST['nomfichier'];
-					$letout = stripslashes($_REQUEST['FCKeditor1']);
-					
-					$fp = fopen($nomfichier,"w");
-					fputs($fp, $letout);
-					fclose($fp);
+<?php
+	if (!isset($_REQUEST['nomfichier']) || !isset($_REQUEST['CKEditor'])) {
+		header('Location: panneau.php');
+		die();
+	}
+?>
 
-					echo $letout;
-				?>
-			</center>
+<?php include("header.php"); ?>
+
+<div id="titre">
+<h2>Editez vos pages</h2>
+</div>
+<center>
+	<?php
+		$nomfichier = $_REQUEST['nomfichier'];
+		$letout = stripslashes($_REQUEST['CKEditor']);
+	
+		$fp = fopen($nomfichier,"w");
+		fputs($fp, $letout);
+		fclose($fp);
+
+		echo $letout;
+	?>
+</center>
+
 <?php include("footer.php"); ?>
