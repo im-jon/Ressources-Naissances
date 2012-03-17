d60347f7e2c7a6212ef81ea33a87342b03ce0dce
Antoine Laroche <antoine_laroche7@hotmail.fr>
2012-03-17 14:01:33 -0400

    Antoine Validation ajout article en cours

--- Article.php
+++ Article.php
@@ -1,4 +1,5 @@
-<?php 
+<?php
+//Antoine Laroche, page qui sert à affiche les articles contenu dans le fichier article.txt
 $titre = "Article";
 include("header.php"); ?>
 <script type="text/javascript">
@@ -11,19 +12,5 @@ $(function() {
 });
 </script>
 	
-	<?php  
-	 include("article.txt");
-# 	   include("Actions//mysql.php");
-# 	   //Affiche l'article le plus récent
-# 	   $requete = "SELECT contenu,titre,source FROM article ORDER BY id;";
-# 	   $resultat = mysql_query($requete);
-# 	   while($article = mysql_fetch_array($resultat))
-# 	   {
-# 		echo '<div class="titre"><a href="#">'.$article["titre"]."</a></br>";
-# 		echo "<reference>".$article["source"]."</reference></div>";
-# 		echo '<div class="desc"><tgauche>'.$article["contenu"]."</tgauche></div></br>";
-# 	   }
-# 	   
-# 	   mysql_close();
-		
+<?php  include("article.txt");	
 	include("footer.php"); ?>
--- traite_ajout_article.php
+++ traite_ajout_article.php
@@ -3,9 +3,29 @@
 //Page qui traite le formulaire pour ajouter un article
 $titre = "Ajout_Article";
 include("header.php");
+
 $titre = $_REQUEST['titre'];
 $desc = $_REQUEST['desc'];
 $source = $_REQUEST['source'];
+// Si la personne a oublié un paramètre on la redirige vers le formulaire pour qu'elle recommence, en lui indiquant le ou les champs oubliés.
+if($titre != " "){
+	echo "Le titre est obligatoire";
+	http_redirect("ajout_article.php");	
+}
+	
+	
+if($desc != " "){
+	echo "Le corps de l'article est obligatoire";
+	http_redirect("ajout_article.php");
+}
+	
+	
+if($source != " "){
+	echo "La source est obligatoire";
+	http_redirect("ajout_article.php");
+}	
+	
+
 
 //Ouverture du fichier qui contient le html. On l'ouvre en append pour ajouter le html à la fin
 if($hfichier = fopen("article.txt", "a")){
